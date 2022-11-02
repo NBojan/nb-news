c3f6b244b92974e13a5838a178247881b280b29f
NBojan <63514753+NBojan@users.noreply.github.com>
2022-11-02 06:20:46 -0500

    title2

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,7 +6,7 @@ const SearchForm = () => {
     return (  
         <section className="search">
             <form onSubmit={e => e.preventDefault()}>
-                <h2 className="mb-20 capitalize titleC">Search hacker news</h2>
+                <h2 className="mb-20 capitalize titleC">Search NB news</h2>
                 <input type="text" value={term} onChange={e => handleSearch(e.target.value)}/>
             </form>
         </section>
